1a6796ae6a9531e8e7f357949e4efac90140c400
Michał <Wyczyn19009@gmail.com>
2024-10-23 15:23:32 +0200

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection.Emit;
+using System.Xml.Linq;
+
+namespace Simulator;
+
+public class Creature
+{
+    public string? Name;
+    public int Level;
+
+    public Creature() { }
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    }
+
+    public string Info()
+    {
+        return $"{Name} [{Level}]";
+    }
+}
