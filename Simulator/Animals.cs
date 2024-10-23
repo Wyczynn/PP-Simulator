666edf1d5dd90bc639e9a65d8435a6e989b0a986
Michał <Wyczyn19009@gmail.com>
2024-10-23 15:56:29 +0200

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -1,12 +1,32 @@
-﻿namespace Simulator;
+﻿using System.Xml.Linq;
+
+namespace Simulator;
 
 public class Animals
 {
-    public required string Description { get; init; }
+    public string description = "";
+    public required string Description
+    {
+        get => description;
+        init
+        {
+            if (value == null)
+                return;
+
+            string temp = String.Join(" ", value.Trim().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries));
+
+            if (temp.Length < 3)
+            {
+                while (temp.Length < 3)
+                {
+                    temp += "#";
+                }
+            }
+            temp = temp[0].ToString().ToUpper() + temp[1..];
+            description = temp[..Math.Min(temp.Length, 15)].TrimEnd();
+        }
+    }
     public uint Size { get; set; } = 3;
 
-    public string Info()
-    {
-        return $"{Description} <{Size}>";
-    }
+    public string Info => $"{Description} <{Size}>";
 }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,12 +1,45 @@
-﻿using System.Reflection.Emit;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Emit;
 using System.Xml.Linq;
 
 namespace Simulator;
 
 public class Creature
 {
-    public string? Name;
-    public int Level;
+
+    private string name = "Unknown";
+    public string? Name
+    {
+        get => name;
+        init
+        {
+            if (value == null)
+                return;
+
+            string temp = String.Join(" ", value.Trim().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries));
+
+            if (temp.Length < 3)
+            {
+                while(temp.Length < 3)
+                {
+                    temp += "#";
+                }
+            }
+            temp = temp[0].ToString().ToUpper() + temp[1..];
+            name = temp[..Math.Min(temp.Length, 25)].TrimEnd();
+        }
+    }
+
+    private int level = 1;
+    public int Level
+    {
+        get => level;
+        init
+        {
+            level = Math.Max(1, Math.Min(10, value));
+        }
+    }
+    public string Info => $"{Name} [{Level}]";
 
     public Creature() { }
 
@@ -21,8 +54,12 @@ public class Creature
         Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
     }
 
-    public string Info()
+
+    public void Upgrade()
     {
-        return $"{Name} [{Level}]";
+        if (level >= 10)
+            return;
+
+        level++;
     }
 }
